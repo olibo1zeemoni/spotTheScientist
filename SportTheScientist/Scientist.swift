dd1a17f0e1e857276b51fa6f30b6ab6f79c12887
Zeemoni <gaboahene1@gmail.com>
2022-02-02 10:30:30 +0000

    first commit

---
+++ SportTheScientist/Scientist.swift
@@ -0,0 +1,17 @@
+//
+//  Scientist.swift
+//  SportTheScientist
+//
+//  Created by Olibo moni on 28/01/2022.
+//
+
+import Foundation
+
+struct Scientist: Codable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
